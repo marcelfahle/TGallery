3035d086c59b7ca2a8fe70dee0799b71a2f88ffe
Marcel Fahle <m.fahle@gmail.com>
2012-11-12 16:38:24 -0500

    adds session redirect on fals login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       redirect_to root_url, notice: "Logged in!"
     else
       flash.now.alert = "Email or password is invalid."
+      render "new"
     end
   end
 
