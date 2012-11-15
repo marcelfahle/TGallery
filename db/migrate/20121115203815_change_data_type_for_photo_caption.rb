e0615ae4f89214582a751ae24adaf5fc957eb7a7
Marcel Fahle <m.fahle@gmail.com>
2012-11-15 15:54:08 -0500

    changed photos migration

--- db/migrate/20121115203815_change_data_type_for_photo_caption.rb
+++ db/migrate/20121115203815_change_data_type_for_photo_caption.rb
@@ -1,7 +1,7 @@
 class ChangeDataTypeForPhotoCaption < ActiveRecord::Migration
   def up
     change_table :photos do |t|
-      t.change :caption, :text, limit: 64.kilobytes
+      t.change :caption, :text
     end
   end
 
