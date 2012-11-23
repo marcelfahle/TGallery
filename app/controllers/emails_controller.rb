77b14ec977f0ae65f381f0e721b5357293ff18fc
Marcel Fahle <m.fahle@gmail.com>
2012-11-23 17:33:25 -0500

    adds remove email function

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,2 +1,16 @@
 class EmailsController < ApplicationController
+
+
+  def destroy
+    @email = Email.find(params[:id])
+    # remove the files
+
+    @email.destroy
+
+    respond_to do |format| 
+      format.html { redirect_to galleries_path }
+      format.json { head :no_content }
+    end
+  end
+
 end
